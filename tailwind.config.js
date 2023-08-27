29e004c81234f8ba43c690c16a130c546b51acef
Arman Karimi <mrevid2@gmail.com>
2023-08-28 01:00:35 +0430

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    screens: {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		screens: {
 			sm: "376px",
 		},
-		fontFamily: {},
-		fontSize: {},
-    extend: {
-      colors: {},
-    },
-  },
-  plugins: [],
-}
+		fontFamily: {
+			"sans-serif": ["Lexend Deca", "sans-serif"],
+			cursive: ["Big Shoulders Display", "cursive"],
+		},
+		fontSize: {
+			"card-header": [
+				"1.5rem",
+				{
+					fontWeight: "700",
+					lineHeight: "3rem",
+				},
+			],
+		},
+		extend: {
+			colors: {
+				primary: {
+					"light-orange": "hsl(31, 77%, 52%)",
+					"dark-cyan": "hsl(184, 100%, 22%)",
+					"very-dark-cyan": "hsl(179, 100%, 13%)",
+				},
+				neutral: {
+					"very-light-gray": "hsl(0, 0%, 95%)",
+					"transparent-white": "hsla(0, 0%, 100%, 0.75)",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
