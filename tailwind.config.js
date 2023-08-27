4e3e720095bc3f943b0e98aa0bc11f6c4c75e54a
Arman Karimi <mrevid2@gmail.com>
2023-08-28 04:19:49 +0430

    Mobile style completed
    commit d5b8cee3fb15bd49a2c1d11eb0963e9745f15ab7
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 04:15:24 2023 +0430
    
        unique button text color for each car card
    
    commit 7e318210ede2591abb98ee96296eb022d8d186bf
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 03:53:50 2023 +0430
    
        card styles as component
    
    commit d1fe08c568d354a0a97b4aff28f7b5f8e90803d9
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 03:50:13 2023 +0430
    
        adjusted body padding
    
    commit f21b4c5dafed89a9f5ec0cab9ec3974b60e8d3d5
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 01:40:42 2023 +0430
    
        styled card
    
        styled the first one, but others are identical so I'll apply it to them.
    
    commit a25e8485c435e253d2a84e3e0148912462e27c6f
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 01:38:56 2023 +0430
    
        adjusted card header
    
    commit 6b3f2a9ed23110b02a7cc81e90687ec243770af5
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 01:25:55 2023 +0430
    
        going to use padding instead of line-height.
        because it has to have top and bottom padding not just bottom.
    
    commit 4b139c9b93724d4250f496b78d05f482b9617914
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 01:08:19 2023 +0430
    
        set correct path to html
    
    commit 2bf313dada48d80cf112c98e09b6d0a0ec47166c
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 01:01:50 2023 +0430
    
        set font values for html & h1 tags
    
    commit 29e004c81234f8ba43c690c16a130c546b51acef
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 01:00:35 2023 +0430
    
        configured tailwind
    
    commit 2acb87637fd39ce43ed696f9d77e002f0d15f41a
    Author: Arman Karimi <mrevid2@gmail.com>
    Date:   Mon Aug 28 00:59:36 2023 +0430
    
        structured html + font links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    screens: {
+	content: ["./*.{html,js}"],
+	theme: {
+		screens: {
 			sm: "376px",
 		},
-		fontFamily: {},
-		fontSize: {},
-    extend: {
-      colors: {},
-    },
-  },
-  plugins: [],
-}
+		fontFamily: {
+			"sans-serif": ["Lexend Deca", "sans-serif"],
+			cursive: ["Big Shoulders Display", "cursive"],
+		},
+		fontSize: {
+			"card-header": [
+				"2.6rem",
+				{
+					fontWeight: "700",
+				},
+			],
+		},
+		extend: {
+			colors: {
+				primary: {
+					"light-orange": "hsl(31, 77%, 52%)",
+					"dark-cyan": "hsl(184, 100%, 22%)",
+					"very-dark-cyan": "hsl(179, 100%, 13%)",
+				},
+				neutral: {
+					"very-light-gray": "hsl(0, 0%, 95%)",
+					"transparent-white": "hsla(0, 0%, 100%, 0.75)",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
